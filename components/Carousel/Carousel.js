0c8f5e262e219780ace459e6582eddac5ec878d1
Orie Chinedu <oriechinedu@gmail.com>
2019-05-17 10:16:50 +0100

    (stretch): Implement carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,49 @@
 class Carousel {
+    constructor(carouselElement) {
+        this.carousel = carouselElement;
+        this.images = this.carousel.querySelectorAll('img');
+        this.imageCount = this.images.length;
+        this.currentImage = 0;
+        this.initCarousel()
+    }
+    initCarousel = () => {
+        this.setImage(this.currentImage)
+        const rightBtn = this.carousel.querySelector('.right-button')
+        const leftBtn = this.carousel.querySelector('.left-button')
+        rightBtn.addEventListener('click', this.nextImage)
+        leftBtn.addEventListener('click', this.prevImage)
+    }
+    nextImage = () => {
+        if (this.currentImage === this.imageCount -1) {
+            this.currentImage = 0;
+        } else {
+            ++this.currentImage;
+        }
+    
+        this.setImage(this.currentImage)
+    }
+    prevImage = () => {
+        if (this.currentImage > 0) {
+            --this.currentImage
+        } else {
+            this.currentImage = this.imageCount -1;
+        }
+        this.setImage(this.currentImage)
+    }
 
+    setImage = (index) => {
+        
+        Array.from(this.images).forEach(img => {
+            img.style.display = 'none';
+        });
+        const currentImage = Array.from(this.images)[index];
+        TweenMax.fromTo(currentImage, 1, {width: 0}, {width: "100%", display: "block"});
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
 
+const carouselObj = new Carousel(carousel)
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
